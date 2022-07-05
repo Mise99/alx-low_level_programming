fdc6523d5d88f0d33d76a4c5adc3cdf75318025b
Mise99 <gladysmise@gmail.com>
2022-07-05 12:57:35 -0700

    task0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * main-main function
  *
@@ -6,16 +7,15 @@
  */
 int main(void)
 {
-	char string[8] = "_putchar";
-	int n = 0;
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 
-	while (n < 8)
-	{
-		_putchar(string[8]);
-		++n;
-	}
-
-	_putchar(10);
 	return(0);
 }	
-
